06ab32a2b02af0e731b81eb5bc85f671929744f5
Matheus H <codelong@proton.me>
2024-07-04 16:28:00 -0300

    Create App.tsx

---
+++ frontend/containers/App.tsx
@@ -0,0 +1,13 @@
+// frontend/containers/App.tsx
+import React from 'react';
+import Editor from './Editor';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
+
+export default App;
